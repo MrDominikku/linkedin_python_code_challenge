bba8b072dd88b8a00db2a6cef3bbc5b965b10258
Dominik <dominik.ruczajewski@gmail.com>
2020-10-20 20:06:05 +0200

    regex pattern update

--- Palindrome/palindrome.py
+++ Palindrome/palindrome.py
@@ -2,17 +2,15 @@ import re
 
 
 def palindrome_validation(validation_string):
-    parse_list = validation_string.split(' ')
-    reverse_string = validation_string.replace(' ', '')[::-1]
 
-    if reverse_string == ''.join(map(str, parse_list)):
-        return True
-    else:
-        return False
+    parse_list = re.sub('[^a-zA-Z ]', '', validation_string).split()
+    reverse_string = re.sub('[^a-zA-Z]', '', validation_string)[::-1]
+
+    return reverse_string.lower() == ''.join(map(str, parse_list)).lower()
 
 
 def only_letters(user_string):
-    match = re.search("[^a-zA-Z ]", user_string)
+    match = re.search("[0-9]", user_string)
     return match is not None
 
 
@@ -24,7 +22,7 @@ def main():
         if user_input == 'quit':
             break
         elif only_letters(user_input):
-            print('Your string contains other characters than A-Z. Try again.')
+            print('Your string contains numbers. Try again.')
         else:
             if palindrome_validation(user_input):
                 print('Your string is palindrome.')
